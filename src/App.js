823e264874b9e04d05adecbd072d9fbd28a626cd
pepsi <pepsi_fack@ukr.net>
2023-01-24 20:12:37 +0200

    feat: restore perfomance App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./scss/app.scss";
 import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
-import PizzaBlock from "./components/PizzaBlock";
+import PizzaBlock from "./components/PizzaBlock/PizzaBlock";
+import Skeleton from "./components/PizzaBlock/Skeleton"
 import React from "react";
 
 
 function App() {
   const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true)
   // console.log(items)
   React.useEffect(() => {
       fetch("https://63c6738ddcdc478e15c1b17b.mockapi.io/items")
@@ -16,7 +18,7 @@ function App() {
     })
     .then((arr) => {
       setItems(arr)
-      // console.log(arr);
+     setIsLoading(false)
     });
   },[])
      
@@ -33,9 +35,12 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {items.map((obj) => (
-              <PizzaBlock {...obj} key={obj.id} />
-            ))}
+            {
+            isLoading ? [...new Array(6)].map((el,index) => <Skeleton key={index}/> ) :
+                items.map((obj,index) => <PizzaBlock {...obj} key={index}/>
+                )
+
+            }
           </div>
         </div>
       </div>
