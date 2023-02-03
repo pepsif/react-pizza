2e9f6694d0a3d6c53c1f40905be31ed66c31b325
pepsi <pepsi_fack@ukr.net>
2023-02-03 09:23:43 +0200

    add: sort in Home.jsx and short edit code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,47 +3,59 @@ import Categories from "../components/Categories";
 import Sort from "../components/Sort";
 import Skeleton from "../components/PizzaBlock/Skeleton";
 import PizzaBlock from "../components/PizzaBlock/PizzaBlock";
-const Home =() =>{    const [items, setItems] = React.useState([]);
-    const [isLoading, setIsLoading] = React.useState(true);
-    const [categoryId,setCategoryId] = React.useState(0);
-    const [sortModalOptionId, setSortActiveModalOption ] = React.useState(0);
-    const sortTypes = ['популярности','цене','алфавиту'];
-    const categoriesTypes = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+const Home = () => {
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [sortModalOptionId, setSortActiveModalOption] = React.useState(0);
+  const sortTypes = ["популярности", "цене", "алфавиту"];
+  const categoriesTypes = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
 
-console.log(sortTypes[sortModalOptionId].популярности )
-    React.useEffect(() => {
-        setIsLoading(true)
-        fetch(`https://63c6738ddcdc478e15c1b17b.mockapi.io/items${
-            categoryId > 0 ? `?category=${categoryId}` : ''
-        } ?sortBy=${ sortModalOptionId === 0 ? 'rating': '' }`  //TODO create fetch correctly
-            // `?sortBy=${sortTypes[sortModalOptionId]}`
-        )
-            .then((response) => {
-                return response.json();
-            })
-            .then((arr) => {
-                setItems(arr)
-                setIsLoading(false)
-            });
-            window.scrollTo(0,0)
-    },[categoryId,sortModalOptionId])
-    return(
-        <div className="container">
-            <div className="content__top">
-                <Categories categoryId={categoryId} setCategoryId={setCategoryId}/>
-                <Sort sortModalOptionId={sortModalOptionId} setSortActiveModalOption={setSortActiveModalOption}/>
-            </div>
-            <h2 className="content__title">Все пиццы</h2>
-            <div className="content__items">
-                {
-                    isLoading ? [...new Array(6)].map((el,index) => <Skeleton key={index}/> ) :
-                        items.map((obj,index) => <PizzaBlock {...obj} key={index}/>
-                        )
+  //    alert(`https://63c6738ddcdc478e15c1b17b.mockapi.io/items?${
+  //     categoryId > 0 ? `category=${categoryId}` : ''}
+  //  &sortBy=${ sortModalOptionId === 0 ? "rating" : ""}  `)
 
-                }
-            </div>
-        </div>
-    )
-}
+  console.log(sortTypes[sortModalOptionId].популярности);
+  React.useEffect(() => {
+    setIsLoading(true);
+    fetch(`https://63c6738ddcdc478e15c1b17b.mockapi.io/items?${
+      categoryId > 0 ? `category=${categoryId}` : ""
+    }&sortBy=${sortModalOptionId === 0 ? "rating" : ""}  //TODO need add sort price and title
+         
+         `)
+      .then((response) => {
+        return response.json();
+      })
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+    window.scrollTo(0, 0);
+  }, [categoryId, sortModalOptionId]);
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Categories categoryId={categoryId} setCategoryId={setCategoryId} />
+        <Sort
+          sortModalOptionId={sortModalOptionId}
+          setSortActiveModalOption={setSortActiveModalOption}
+        />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((el, index) => <Skeleton key={index} />)
+          : items.map((obj, index) => <PizzaBlock {...obj} key={index} />)}
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
